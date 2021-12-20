1d4375a5b0bcadcaee6970508a5f0b9820500450
Amogh-Bharadwaj <amoghrb01@gmail.com>
2021-12-20 15:39:42 +0530

    Finishing touches

--- Night-Diary/src/App.js
+++ Night-Diary/src/App.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import logo from './logo.svg';
 import './App.css';
 import Entry from "./entry"
 import Diary from "./diary";
